5a28d5c426a26a4777cf2acde09bd24c65fe0f15
fastilov3 <50419270+fastilov3@users.noreply.github.com>
2020-10-11 22:35:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let a = 1;
+    
+    while(a == 1) {
+        let res = str;
+        bracketsConfig.map(n => str = str.replace(n[0] + n[1], ''));
+        if (res === str) { 
+          a = 0; 
+        };
+    }
+    return (str.length == 0) ? true : false;
+  }
